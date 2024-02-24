26acc8e3a92da227715a79e4a10a859b72956cb2
Louis Franck <louisfranck.moussima@esprit.tn>
2024-02-24 21:02:19 +0100

    modification added Jacoco and testing Contrat

---
+++ src/test/java/tn/esprit/spring/kaddem/Test/ContratTest.java
@@ -0,0 +1,183 @@
+package tn.esprit.spring.kaddem.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.entities.Specialite;
+import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+import tn.esprit.spring.kaddem.services.ContratServiceImpl;
+import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
+import tn.esprit.spring.kaddem.services.IContratService;
+import tn.esprit.spring.kaddem.services.IEtudiantService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ContratTest {
+    @InjectMocks
+    IContratService iContratService = new IContratService() {
+        @Override
+        public List<Contrat> retrieveAllContrats() {
+            return null;
+        }
+
+        @Override
+        public Contrat updateContrat(Contrat ce) {
+            return null;
+        }
+
+        @Override
+        public Contrat addContrat(Contrat ce) {
+            return null;
+        }
+
+        @Override
+        public Contrat retrieveContrat(Integer idContrat) {
+            return null;
+        }
+
+        @Override
+        public void removeContrat(Integer idContrat) {
+
+        }
+
+        @Override
+        public Contrat affectContratToEtudiant(Integer idContrat, String nomE, String prenomE) {
+            return null;
+        }
+
+        @Override
+        public Integer nbContratsValides(Date startDate, Date endDate) {
+            return null;
+        }
+
+        @Override
+        public float getChiffreAffaireEntreDeuxDates(Date startDate, Date endDate) {
+            return 0;
+        }
+
+        @Override
+        public void retrieveAndUpdateStatusContrat() {
+
+        }
+    };
+
+    public ContratTest() throws ParseException {
+    }
+
+    //Contrat
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Mock
+    ContratServiceImpl iContratServices = Mockito.mock(ContratServiceImpl.class);
+
+    @Mock
+    ContratRepository contratRepository= Mockito.mock(ContratRepository.class);
+    @InjectMocks
+    ContratServiceImpl contratService;
+
+    Etudiant etudiant= new Etudiant("cadal","evans");
+    // Create a sample contrat object with specific start and end dates
+    Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse("2024-01-01");
+    Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2024-12-31");
+    Contrat contrat = new Contrat(1, startDate, endDate, Specialite.IA, false, 100);
+
+    //Contrat contrat=new Contrat(1,new Date(),new Date(), Specialite.IA,false,100);
+
+    List<Contrat> listOfContrat = new ArrayList<Contrat>(){
+        {
+            add(new Contrat(1, startDate, endDate, Specialite.SECURITE,false,1200));
+            add(new Contrat(2, startDate, endDate, Specialite.RESEAUX,false,1002));
+        }
+    };
+
+
+    // Test Begin
+    @Test
+    public void testDeleteContrat() {
+        // Mocking behavior to return optional containing a Contrat object
+        when(contratRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(new Contrat(1, startDate, endDate, Specialite.SECURITE,false,1200))); // Create a new Contrat object for testing
+
+        // Calling the removeContrat method
+        iContratService.removeContrat(2);
+
+
+        // Verifying that the repository's delete method was called with the correct argument
+        //Mockito.verify(contratRepository).delete(any(Contrat.class)); // Verifying that delete method is called with any Contrat object
+    }
+
+
+    @Test
+    public void testAddContrat() {
+        // Given
+
+        Contrat e = new Contrat(1,  startDate, endDate, Specialite.IA, false, 100);
+        // Mocking the behavior of contratRepository.save()
+        when(iContratServices.addContrat(e)).thenReturn(e);
+
+        // When
+        Contrat addedContrat = iContratServices.addContrat(e);
+
+        // Then
+        Assertions.assertNotNull(addedContrat);
+        assertEquals(e, addedContrat);
+    }
+
+    @Test
+    public void testRetrieveContrat() {
+        // Mocking behavior to return optional containing a Contrat object
+        when(contratRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(contrat));
+
+        // Call the method to be tested
+        Contrat retrievedContrat = iContratServices.retrieveContrat(2);
+        when(iContratServices.retrieveContrat(2)).thenReturn(contrat);
+
+        // Verify that the retrieved contrat is not null
+        //Assertions.assertNotNull(retrievedContrat);
+    }
+
+    /*
+    @Test
+    public void testUpdateContrat() {
+        // Mocking behavior to return optional containing a Contrat object
+        when(contratRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(contrat));
+
+        // Creating a new Contrat object with updated information
+        Contrat updatedContrat = new Contrat(1, startDate, endDate, Specialite.CLOUD, false, 1000);
+
+        // Mocking behavior of the save method of contratRepository
+        when(contratRepository.save(updatedContrat)).thenReturn(updatedContrat);
+
+        // Calling the updateDepartement method with the updatedDepartement object
+        Contrat updated = iContratServices.updateContrat(updatedContrat);
+
+        // Assert that the updated contrat is not null
+        Assertions.assertNotNull(updated);
+
+        // Assert that the montantContrat of the updated contrat matches the expected value
+        assertEquals(1000, updated.getMontantContrat());
+
+
+    }
+
+     */
+
+
+}
--- src/test/java/tn/esprit/spring/kaddem/Test/EtudiantTest.java
+++ src/test/java/tn/esprit/spring/kaddem/Test/EtudiantTest.java
@@ -1,4 +1,201 @@
 package tn.esprit.spring.kaddem.Test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.entities.Equipe;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.entities.Niveau;
+import tn.esprit.spring.kaddem.repositories.DepartementRepository;
+import tn.esprit.spring.kaddem.repositories.EquipeRepository;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+import tn.esprit.spring.kaddem.services.EquipeServiceImpl;
+import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
+import tn.esprit.spring.kaddem.services.IEquipeService;
+import tn.esprit.spring.kaddem.services.IEtudiantService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
 public class EtudiantTest {
+    @InjectMocks
+    IEtudiantService iEtudiantService = new IEtudiantService() {
+        @Override
+        public List<Etudiant> retrieveAllEtudiants() {
+            return null;
+        }
+
+        @Override
+        public Etudiant addEtudiant(Etudiant e) {
+            return null;
+        }
+
+        @Override
+        public Etudiant updateEtudiant(Etudiant e) {
+            return null;
+        }
+
+        @Override
+        public Etudiant retrieveEtudiant(Integer idEtudiant) {
+            return null;
+        }
+
+        @Override
+        public void removeEtudiant(Integer idEtudiant) {
+
+        }
+
+        @Override
+        public void assignEtudiantToDepartement(Integer etudiantId, Integer departementId) {
+
+        }
+
+        @Override
+        public Etudiant addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe) {
+            return null;
+        }
+
+        @Override
+        public List<Etudiant> getEtudiantsByDepartement(Integer idDepartement) {
+            return null;
+        }
+    };
+
+    // Create a mock object for IEquipeService Etudiant
+    @Mock
+    IEtudiantService iEtudiantServices = Mockito.mock(IEtudiantService.class);
+    @Test
+    @Order(2)
+    public void testRetrievAllEtudiant(){
+        // Mocking the behavior of equipeRepository.findAll() method
+        when(iEtudiantServices.retrieveAllEtudiants()).thenReturn(Collections.emptyList());
+
+
+
+        List<Etudiant> EtudiantList = etudiantService.retrieveAllEtudiants();
+        assertEquals(0, EtudiantList.size());
+    }
+
+    @Test
+    public void testDeleteEtudiant() {
+        // Mocking behavior to return optional containing a Departement object
+        when(etudiantRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(etudiant));
+
+        // Calling the deleteDepartement method
+        etudiantService.removeEtudiant(2);
+
+        // Verifying that the repository's delete method was called with the correct argument
+        Mockito.verify(etudiantRepository).delete(etudiant);
+    }
+
+    @Mock
+    EtudiantRepository etudiantRepository= Mockito.mock(EtudiantRepository.class);
+    @InjectMocks
+    EtudiantServiceImpl etudiantService;
+
+    Etudiant etudiant= new Etudiant("cadal","evans");
+
+    List<Etudiant> listOfEtudiant = new ArrayList<Etudiant>(){
+        {
+            add(new Etudiant("renne","irisse"));
+            add(new Etudiant("lyon","maltois"));
+        }
+    };
+
+    @Test
+    public void testRetrieveEtudiant() {
+        when(etudiantRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(etudiant));
+        Etudiant etudiant1 = etudiantService.retrieveEtudiant(2);
+        Assertions.assertNotNull(etudiant1);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testAddEtudiant() {
+        // Given
+        Etudiant e = new Etudiant("Franck", "Louis");
+
+        // Mocking the behavior of departementRepository.save()
+        when(etudiantRepository.save(e)).thenReturn(e);
+
+        // When
+        Etudiant addedEtudiant = etudiantService.addEtudiant(e);
+
+        // Then
+        Assertions.assertNotNull(addedEtudiant);
+        assertEquals(e, addedEtudiant);
+        // You can add more assertions if needed
+    }
+    @Test
+    public void testUpdateEtudiant() {
+        // Mocking behavior to return optional containing a Departement object
+        when(etudiantRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(etudiant));
+
+        // Creating a new Departement object with updated information
+        Etudiant updatedEtudiant = new Etudiant("nimes","senior" );
+
+        // Mocking behavior of the save method of departementRepository
+        when(etudiantRepository.save(updatedEtudiant)).thenReturn(updatedEtudiant);
+
+        // Calling the updateDepartement method with the updatedDepartement object
+        Etudiant updated = etudiantService.updateEtudiant(updatedEtudiant);
+
+        // Asserting that the updatedDepartement object is not null
+        Assertions.assertNotNull(updated);
+
+        // Asserting that the name of the updated department matches the expected value
+        assertEquals("nimes", updated.getNomE());
+    }
+
+    @Mock
+    private DepartementRepository departementRepository;
+
+    @Test
+    public void testGetEtudiantsByDepartement() {
+        // Mocking data
+        Integer idDepartement = 1; // Sample departement ID
+        Departement departement = new Departement(1,"physiques"); // Create a sample departement object
+        departement.setIdDepart(idDepartement);
+
+        Etudiant etudiant1 = new Etudiant("elm","ralph"); // Sample etudiant object 1
+        Etudiant etudiant2 = new Etudiant("violette","elise"); // Sample etudiant object 2
+
+        List<Etudiant> expectedEtudiants = new ArrayList<>();
+        expectedEtudiants.add(etudiant1);
+        expectedEtudiants.add(etudiant2);
+
+        // Mocking the behavior of etudiantRepository.findEtudiantsByDepartement_IdDepart()
+        when(etudiantRepository.findEtudiantsByDepartement_IdDepart(idDepartement)).thenReturn(expectedEtudiants);
+
+        // Calling the method to be tested
+        List<Etudiant> actualEtudiants = etudiantService.getEtudiantsByDepartement(idDepartement);
+
+        // Verifying that the repository method was called with the correct argument
+        // In this case, we don't need to explicitly verify the argument since we're using anyInt()
+        // However, it's good practice to do so for clarity
+        //verify(etudiantRepository).findEtudiantsByDepartement_IdDepart(idDepartement);
+
+        // Verifying the size and contents of the returned list
+        assertEquals(expectedEtudiants.size(), actualEtudiants.size());
+        assertEquals(expectedEtudiants, actualEtudiants);
+    }
+
 }
