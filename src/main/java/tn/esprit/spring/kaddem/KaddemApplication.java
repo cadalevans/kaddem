b0f69c8b7d2e889cebc5705d93f33650bf83e13b
Louis Franck <louisfranck.moussima@esprit.tn>
2024-02-25 12:48:29 +0100

    src junit

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -1,6 +1,6 @@
 package tn.esprit.spring.kaddem;
 
-import org.junit.Test;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
