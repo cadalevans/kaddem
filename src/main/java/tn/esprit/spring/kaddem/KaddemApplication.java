3696d9b69fcaea71a646ea89178f3a626dbf6511
Louis Franck <louisfranck.moussima@esprit.tn>
2024-02-25 10:41:38 +0100

    Jenkisfile src

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class KaddemApplication {
 
-    @Test
+
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
